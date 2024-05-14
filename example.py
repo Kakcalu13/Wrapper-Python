58202dd5eda8085dc64bcb69263f090df6cdfb8d
kakcalu13 <kevin.a.araujo@gmail.com>
2024-05-13 23:24:08 -0400

    testing code

--- example.py
+++ example.py
@@ -36,7 +36,9 @@
 from mistyPy import Robot
 
 # TODO: Replace with your IP
-misty = Robot("192.168.0.31") # This is the IP of my misty. Replace with your IP
-misty.changeLED(0, 0, 255)
+misty = Robot("192.168.10.128") # This is the IP of my misty. Replace with your IP
+misty.changeLED(255, 0, 255)
 misty.moveHeadPosition(0, 0, 0, 100) # center the head
 misty.moveArmsDegrees(0, 0, 100, 100)
+misty.battery()
+misty.populateImages()
